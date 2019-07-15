8be006e01fdcdec4c6bd236f85c909a356587a8e
ram <krishnat.0111@gmail.com>
2019-07-15 11:47:20 -0400

    a

---
+++ src/main/java/mensfashion/App.java
@@ -0,0 +1,13 @@
+package mensfashion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
